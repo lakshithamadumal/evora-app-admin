f962322d68ef30d3309b7347a825470ef211b243
Heshan Nawarathna <117019222+heshannawarathne@users.noreply.github.com>
2026-03-27 20:48:15 +0530

    users
    userAdapter and UsersFragment

--- app/src/main/java/com/udari/evoraadmin/model/UserModel.java
+++ app/src/main/java/com/udari/evoraadmin/model/UserModel.java
@@ -1,8 +1,12 @@
 package com.udari.evoraadmin.model;
 
 public class UserModel {
-    String name, mobile, city;
-    int orderCount;
+    private String name, mobile, city;
+    private int orderCount;
+
+
+    public UserModel() {
+    }
 
     public UserModel(String name, String mobile, String city, int orderCount) {
         this.name = name;
@@ -11,9 +15,15 @@ public class UserModel {
         this.orderCount = orderCount;
     }
 
-    // Getters (Adapter එකට දත්ත ගන්න ඕනේ නිසා)
+    // Getters
     public String getName() { return name; }
     public String getMobile() { return mobile; }
     public String getCity() { return city; }
     public int getOrderCount() { return orderCount; }
+
+
+    public void setName(String name) { this.name = name; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
+    public void setCity(String city) { this.city = city; }
+    public void setOrderCount(int orderCount) { this.orderCount = orderCount; }
 }
